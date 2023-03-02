5640045095821556e7a9cd3f520f0c46eedd414f
unknown <sohailshahidi1435@gmail.com>
2023-03-02 12:52:37 +0430

    Add Calculate logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+
+  const clickHandler = (e) => {
+    setState((state) => calculate(state, e.target.value));
+  };
   return (
     <section>
       <div className="result">
-        <p>0</p>
+        <p>
+          {state.total}
+          {state.operation}
+          {state.next}
+        </p>
       </div>
       <div className="calculator">
-        <button type="button" className="button" value="AC">AC</button>
-        <button type="button" className="button" value="AC">+/-</button>
-        <button type="button" className="button" value="%">%</button>
-        <button type="button" className="operator" value="÷">÷</button>
-        <button type="button" className="button" value="7">7</button>
-        <button type="button" className="button" value="8">8</button>
-        <button type="button" className="button" value="9">9</button>
-        <button type="button" className="operator" value="*">*</button>
-        <button type="button" className="button" value="4">4</button>
-        <button type="button" className="button" value="5">5</button>
-        <button type="button" className="button" value="6">6</button>
-        <button type="button" className="operator" value="-"> -</button>
-        <button type="button" className="button" value="3">3</button>
-        <button type="button" className="button" value="2">2</button>
-        <button type="button" className="button" value="1">1</button>
-        <button type="button" className="operator" value="+">+</button>
-        <button type="button" className="button btn-0" value="0">0</button>
-        <button type="button" className="button" value=".">.</button>
-        <button type="button" className="operator" value="="> =</button>
+        <button type="button" className="button" onClick={clickHandler} value="AC">AC</button>
+        <button type="button" className="button" onClick={clickHandler} value="AC">+/-</button>
+        <button type="button" className="button" onClick={clickHandler} value="%">%</button>
+        <button type="button" className="operator" onClick={clickHandler} value="÷">÷</button>
+        <button type="button" className="button" onClick={clickHandler} value="7">7</button>
+        <button type="button" className="button" onClick={clickHandler} value="8">8</button>
+        <button type="button" className="button" onClick={clickHandler} value="9">9</button>
+        <button type="button" className="operator" onClick={clickHandler} value="x">x</button>
+        <button type="button" className="button" onClick={clickHandler} value="4">4</button>
+        <button type="button" className="button" onClick={clickHandler} value="5">5</button>
+        <button type="button" className="button" onClick={clickHandler} value="6">6</button>
+        <button type="button" className="operator" onClick={clickHandler} value="-"> -</button>
+        <button type="button" className="button" onClick={clickHandler} value="3">3</button>
+        <button type="button" className="button" onClick={clickHandler} value="2">2</button>
+        <button type="button" className="button" onClick={clickHandler} value="1">1</button>
+        <button type="button" className="operator" onClick={clickHandler} value="+">+</button>
+        <button type="button" className="button btn-0" onClick={clickHandler} value="0">0</button>
+        <button type="button" className="button" onClick={clickHandler} value=".">.</button>
+        <button type="button" className="operator" onClick={clickHandler} value="="> =</button>
       </div>
     </section>
   );
 }
+
+
 
 export default Calculator;
