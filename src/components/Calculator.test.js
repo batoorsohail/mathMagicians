dd89c8e51e15c847a685cec5957c789a86e50d8e
unknown <sohailshahidi1435@gmail.com>
2023-03-08 22:20:44 +0430

    Fix Linters error

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,90 +1,91 @@
-import { render, screen, act } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import renderer from "react-test-renderer";
-import Calculator from "./Calculator";
-it("renders correctly", () => {
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
+import Calculator from './Calculator';
+
+it('renders correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-describe("User interactions", () => {
-  test("if the correct special key is passed for clearing data", () => {
+describe('User interactions', () => {
+  test('if the correct special key is passed for clearing data', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("AC");
+      buttonElement = screen.getByText('AC');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("AC");
+    expect(outputElement.textContent).toBe('AC');
   });
-  test("if the right special key is passed for equality", () => {
+  test('if the right special key is passed for equality', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("=");
+      buttonElement = screen.getByText('=');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("=");
+    expect(outputElement.textContent).toBe('=');
   });
-  test("if the right special key is passed for sign change", () => {
+  test('if the right special key is passed for sign change', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("+/-");
+      buttonElement = screen.getByText('+/-');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("+/-");
+    expect(outputElement.textContent).toBe('+/-');
   });
-  test("if the right operation is passed for addition", () => {
+  test('if the right operation is passed for addition', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("+");
+      buttonElement = screen.getByText('+');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("+");
+    expect(outputElement.textContent).toBe('+');
   });
-  test("if the right operation is passed for subtraction", () => {
+  test('if the right operation is passed for subtraction', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("-");
+      buttonElement = screen.getByText('-');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("-");
+    expect(outputElement.textContent).toBe('-');
   });
-  test("if the right operation is passed for multiplication", () => {
+  test('if the right operation is passed for multiplication', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("x");
+      buttonElement = screen.getByText('x');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("x");
+    expect(outputElement.textContent).toBe('x');
   });
-  test("if the right operation is passed for division", () => {
+  test('if the right operation is passed for division', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("÷");
+      buttonElement = screen.getByText('÷');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("÷");
+    expect(outputElement.textContent).toBe('÷');
   });
-  test("if the right operation is passed for modulo", () => {
+  test('if the right operation is passed for modulo', () => {
     render(<Calculator />);
     let buttonElement;
     act(() => {
-      buttonElement = screen.getByText("%");
+      buttonElement = screen.getByText('%');
       userEvent.click(buttonElement);
     });
     const outputElement = buttonElement;
-    expect(outputElement.textContent).toBe("%");
+    expect(outputElement.textContent).toBe('%');
   });
 });
